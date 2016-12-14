be291ffefabc01cf42912027b12e0de0be86b88f
jalenkjx <jinlei569@gmail.com>
2016-12-14 19:31:02 +0800

    修复无收货地址时，无合计的bug

--- src/js/orderAjax.js
+++ src/js/orderAjax.js
@@ -78,115 +78,116 @@ define(['jquery','pingpp'],function($,pingpp){
 						$('.info_address').children('span').html(data.addressName);
 					}
 				}//遍历datas；
-				
-				
-				
-				
-			    //创建订单前数据准备接口
-				$.ajax({
-			    	type:"post",
-			    	url:api+"v1/api/shop/order/preview",
-			    	//url:"http://service.myzhenzhen.com/zzht/v1/api/shop/order/preview",
-			    	data:beforeOrder,
-			    	headers:{
-			    		'Authorization':'Bearer '+token,
-						'Content-Type': 'application/json'
-			    	},
-			    	success:function(res){
-			    		//console.log(res);
-			    		
-			    		//支付商品的信息
-			    		var payinfos = JSON.stringify(res.datas.payGoodsInfos);
-			    		
-			    		//总运费(获取为分)
-			    		var cost = res.datas.orderExpressAmount/100;
-			    		
-			    		$('.cost').children('p').children('span').html(cost);
-			    		
-			    		//总价 合计
-			    		var total = res.datas.orderAmount
-			    		$('footer').children('p').children('span').children('i').html(total/100);
-			    		//默认渠道为alipay;
-			    		var channel = 'alipay_wap';
-			    		//默认创建订单参数
-		    		    var objOrder = {
-								"order":{
-									"buyerId":userId,
-									"sellerId":sellerId,
-									"amount":total,
-									"currency":"cny",
-									"channel":channel,
-									"clientIp":ip,
-									"addressId":addrId,
-									"payGoodsInfo":payinfos
-								}
-							}
-					    var order = JSON.stringify(objOrder);
-						//支付方式选择
-						$('li','.pay_method').on('click',function(e){
-							e.stopPropagation();
-							$(this).addClass('active').siblings('li').removeClass('active');
-							//选择支付渠道改变订单参数;
-							if($('li.active').index() == 1){
-								channel = 'wx_wap';
-							}else{
-								channel ='alipay_wap';
-							}
-							//创建订单的参数改变;
-							objOrder = {
-								"order":{
-									"buyerId":userId,
-									"sellerId":sellerId,
-									"amount":total,
-									"currency":"cny",
-									"channel":channel,
-									"clientIp":ip,
-									"addressId":addrId,
-									"payGoodsInfo":payinfos
-								}
-							}
-							order = JSON.stringify(objOrder);
-						});
-			    		//点击结算
-						$('button','footer').on('click',function(e){
-							$(this).attr('disabled','disabled');
-							$(this).css('background','#858585');
-							$(this).html('正在结算...');
-							e.stopPropagation();
-							//创建订单接口
-							$.ajax({
-								type:"POST",
-								url:api+"v1/api/shop/order",
-								//url:'http://192.168.199.113/zzht/v1/api/shop/order',
-								//url:"http://service.myzhenzhen.com/zzht/v1/api/shop/order",
-								data:order,
-								crossDomain: true,
-								dataType:'json',
-								headers:{
-									'Authorization':'Bearer '+token,
-									'Content-Type': 'application/json'
-								},
-								success:function(res){
-									console.log(res);
-									//ping++
-									pingpp.createPayment(res.datas, function(result, err){
-										console.log(result);
-										console.log(err.msg);
-    									console.log(err.extra);
-										if (result == "success") {
-										    // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL。
-										} else if (result == "fail") {
-										    // charge 不正确或者微信公众账号支付失败时会在此处返回
-										} else if (result == "cancel") {
-										    // 微信公众账号支付取消支付
-									  }
-									});
-								}
-							});
-						});//结算按钮点击事件	
-			    	}//创建订单前的数据准备接口 success;
-			    });//创建订单前的数据准备接口
 			}//else	
 		}//收货地址ajax success
 	});//收货地址ajax
+	
+	$.ajax({
+    	type:"post",
+    	url:api+"v1/api/shop/order/preview",
+    	//url:"http://service.myzhenzhen.com/zzht/v1/api/shop/order/preview",
+    	data:beforeOrder,
+    	headers:{
+    		'Authorization':'Bearer '+token,
+			'Content-Type': 'application/json'
+    	},
+    	success:function(res){
+    		//console.log(res);
+    		
+    		//支付商品的信息
+    		var payinfos = JSON.stringify(res.datas.payGoodsInfos);
+    		
+    		//总运费(获取为分)
+    		var cost = res.datas.orderExpressAmount/100;
+    		
+    		$('.cost').children('p').children('span').html(cost);
+    		
+    		//总价 合计
+    		var total = res.datas.orderAmount
+    		$('footer').children('p').children('span').children('i').html(total/100);
+    		//默认渠道为alipay;
+    		var channel = 'alipay_wap';
+    		//默认创建订单参数
+		    var objOrder = {
+					"order":{
+						"buyerId":userId,
+						"sellerId":sellerId,
+						"amount":total,
+						"currency":"cny",
+						"channel":channel,
+						"clientIp":ip,
+						"addressId":addrId,
+						"payGoodsInfo":payinfos
+					}
+				}
+		    var order = JSON.stringify(objOrder);
+			//支付方式选择
+			$('li','.pay_method').on('click',function(e){
+				e.stopPropagation();
+				$(this).addClass('active').siblings('li').removeClass('active');
+				//选择支付渠道改变订单参数;
+				if($('li.active').index() == 1){
+					channel = 'wx_wap';
+				}else{
+					channel ='alipay_wap';
+				}
+				//创建订单的参数改变;
+				objOrder = {
+					"order":{
+						"buyerId":userId,
+						"sellerId":sellerId,
+						"amount":total,
+						"currency":"cny",
+						"channel":channel,
+						"clientIp":ip,
+						"addressId":addrId,
+						"payGoodsInfo":payinfos
+					}
+				}
+				order = JSON.stringify(objOrder);
+			});
+    		//点击结算
+			$('button','footer').on('click',function(e){
+				if($('.add_address').css('display')=='block'){
+					alert('请添加收货人信息');
+				}else{
+					$(this).attr('disabled','disabled');
+					$(this).css('background','#858585');
+					$(this).html('正在结算...');
+					e.stopPropagation();
+					//创建订单接口
+					$.ajax({
+						type:"POST",
+						url:api+"v1/api/shop/order",
+						//url:'http://192.168.199.113/zzht/v1/api/shop/order',
+						//url:"http://service.myzhenzhen.com/zzht/v1/api/shop/order",
+						data:order,
+						crossDomain: true,
+						dataType:'json',
+						headers:{
+							'Authorization':'Bearer '+token,
+							'Content-Type': 'application/json'
+						},
+						success:function(res){
+							console.log(res);
+							//ping++
+							pingpp.createPayment(res.datas, function(result, err){
+								console.log(result);
+								console.log(err.msg);
+								console.log(err.extra);
+								if (result == "success") {
+								    // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的支付结果都会跳转到 extra 中对应的 URL。
+								} else if (result == "fail") {
+								    // charge 不正确或者微信公众账号支付失败时会在此处返回
+								} else if (result == "cancel") {
+								    // 微信公众账号支付取消支付
+							  }
+							});
+						}
+					})
+				
+				}
+			});//结算按钮点击事件	
+    	}//创建订单前的数据准备接口 success;
+    });//创建订单前的数据准备接口
 })//define
