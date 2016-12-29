d720172c17f49e8445d0751872d561efad7de856
jalenkjx <jinlei569@gmail.com>
2016-12-29 10:33:57 +0800

    domins_imgLink

--- src/js/param.js
+++ src/js/param.js
@@ -2,17 +2,16 @@ define(['jquery'],function(){
 	 //api = 'http://zhenzhen.s1.natapp.cc/zzht/';
 	api = 'http://192.168.199.127/zzht/';
 	// api = 'http://service.myzhenzhen.com/zzht/';
-	 imgLink = 'http://o6uda1nl0.bkt.clouddn.com/';//内网
+	//imgLink = 'http://o6uda1nl0.bkt.clouddn.com/';//内网
 	// imgLink = 'http://7xrr05.com1.z0.glb.clouddn.com/';//外网
 	
 	$.ajax({
 		type:"get",
 		url:api+"v1/api/domain/get_domains",
 		success:function(res){
-			console.log(res);
-			$.each(res.datas,function(i){
-				
-			})
+			//console.log(res);
+			//console.log(res.datas[1].urls[0]);
+			imgLink = res.datas[1].urls[0];
 		}
 	});
 })
